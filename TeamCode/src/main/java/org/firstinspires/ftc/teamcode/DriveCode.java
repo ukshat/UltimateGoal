f0a00420f0948363d5671797ea468d1e6fd8b135
ukshat <33008157+ukshat@users.noreply.github.com>
2020-10-02 13:18:50 -0700

    Drive Code debugged, reconfigured, and tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode.java
@@ -5,8 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+// Register this Op Mode on the Android phone
+@TeleOp (name = "Drive Code", group = "Op Modes")
 public class DriveCode extends LinearOpMode {
 
+    // Create the four motors, one for each mecanum wheel
     private DcMotor tl_motor;
     private DcMotor tr_motor;
     private DcMotor bl_motor;
@@ -14,46 +17,71 @@ public class DriveCode extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        tl_motor = hardwareMap.dcMotor.get("tl_motor");
-        tr_motor = hardwareMap.dcMotor.get("tr_motor");
-        bl_motor = hardwareMap.dcMotor.get("bl_motor");
-        br_motor = hardwareMap.dcMotor.get("br_motor");
-
-        tr_motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        br_motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Find each motor on the hardware map
+        tl_motor = hardwareMap.dcMotor.get("LeftFront");
+        tr_motor = hardwareMap.dcMotor.get("RightFront");
+        bl_motor = hardwareMap.dcMotor.get("LeftRear");
+        br_motor = hardwareMap.dcMotor.get("RightRear");
 
         waitForStart();
 
         while(opModeIsActive()){
 
+            // Create variables to hold the direction that the left stick was moved
             double xPos = gamepad1.left_stick_x;
             double yPos = gamepad1.left_stick_y;
+
+            // Create a variable to hold the amount of rotation
             double rot = gamepad1.right_stick_x;
 
-            double tlPower = xPos + yPos + rot;
-            double trPower = xPos - yPos - rot;
-            double blPower = xPos - yPos + rot;
-            double brPower = xPos + yPos - rot;
-
-            double[] powers = {tlPower, trPower, blPower, brPower};
-            double largestPower = findLargest(powers);
-
-            double scalar;
-            if(largestPower > 1 || largestPower < -1){
-                scalar = 1 / Math.abs(largestPower);
-            }
-            else {
-                scalar = 1;
-            }
-
-            tl_motor.setPower(scalar * tlPower);
-            tr_motor.setPower(scalar * trPower);
-            bl_motor.setPower(scalar * blPower);
-            br_motor.setPower(scalar * brPower);
+            double[] powers = calculateMotorPower(xPos, yPos, rot);
+            // Apply the scalar to each of the powers and set the motors to that power
+            tl_motor.setPower(powers[0]);
+            tr_motor.setPower(powers[1]);
+            bl_motor.setPower(powers[2]);
+            br_motor.setPower(powers[3]);
 
         }
     }
-    public double findLargest(double[] powers){
+
+    public static double[] calculateMotorPower(double x, double y, double rotation) {
+        // Assign an amount of power to variable of each motor
+        double tlPower = x + y + rotation;
+        double trPower = -x + y - rotation;
+        double blPower = -x + y + rotation;
+        double brPower = x + y - rotation;
+
+        // In the case that any power is out of the bounds of setPower(), we have to scale
+        // all powers down so that the largest power is exactly 1, and the rest are
+        // proportional to the largest power
+
+        // Find the largest power of the four powers
+        double[] powers = {tlPower, trPower, blPower, brPower};
+        double largestPower = findLargest(powers);
+
+        // Create a variable to scale each power down
+        double normalizer;
+
+        // If the largest power happens to be out of bounds, assign to scalar a value such
+        // that the largest power will be scaled down to exactly one
+        if(largestPower > 1 || largestPower < -1){
+            normalizer = 1 / Math.abs(largestPower);
+        }
+
+        // If the largest power is not out of bounds, there is no need to adjust values
+        else {
+            normalizer = 1;
+        }
+
+        return new double[]{(normalizer * tlPower), (normalizer * trPower), (normalizer * blPower), (normalizer * brPower)};
+    }
+
+    /**
+     * Returns the largest value out of a double array
+     * @param powers   an array of motor powers
+     * @return         the largest power out of the array
+     */
+    public static double findLargest(double[] powers){
         double largest = powers[0];
         for(double d: powers){
             if(d > largest){
