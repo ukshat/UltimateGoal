fe3d6429e0d801580285c177a5cf472964c2174f
ukshat <33008157+ukshat@users.noreply.github.com>
2020-10-07 14:32:50 -0700

    Autonomous using Encoders created, telemetry added to Drive Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousByEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousByEncoders.java
@@ -38,9 +38,36 @@ public class AutonomousByEncoders extends LinearOpMode {
         // 304.8 mm is how much we want to move
         // 0.22165663 mm is the length of one tick
 
+        double distance = 304.8;
+
         fl_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         fr_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         bl_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         br_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fl_motor.setTargetPosition((int)(distance / TICK_LENGTH));
+        fr_motor.setTargetPosition((int)(distance / TICK_LENGTH));
+        bl_motor.setTargetPosition((int)(distance / TICK_LENGTH));
+        br_motor.setTargetPosition((int)(distance / TICK_LENGTH));
+
+        fl_motor.setPower(-1);
+        fr_motor.setPower(1);
+        bl_motor.setPower(1);
+        br_motor.setPower(-1);
+
+        moving();
+
+        fl_motor.setPower(0);
+        fr_motor.setPower(0);
+        bl_motor.setPower(0);
+        br_motor.setPower(0);
     }
+
+    void moving(){
+        while (fl_motor.isBusy() || fr_motor.isBusy() || bl_motor.isBusy() || br_motor.isBusy()){
+
+        }
+        return;
+    }
+
 }
