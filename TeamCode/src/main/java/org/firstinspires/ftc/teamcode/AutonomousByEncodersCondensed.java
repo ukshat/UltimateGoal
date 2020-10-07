da5707e0fc224ca8853874eae672ea06889d46cf
Siddharth Kadari <skadari270@student.fuhsd.org>
2020-10-07 15:31:31 -0700

    Condensed Autonomous using Encoders (all movement code is condensed into one method):
    move(double degrees, double distance, double power)
    
    Linear and diagonal movement, rotational/orbital movement not included

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousByEncodersCondensed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousByEncodersCondensed.java
@@ -5,67 +5,67 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class AutonomousByEncodersCondensed extends LinearOpMode {
 
-    DcMotor fl_motor;
-    DcMotor fr_motor;
-    DcMotor bl_motor;
-    DcMotor br_motor;
+    DcMotor[/*Front Left, Front Right, Back Left, Back Right*/] motors;
 
     static final double TICK_LENGTH = 0.22165663;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        fl_motor = hardwareMap.dcMotor.get("LeftFront");
-        fr_motor = hardwareMap.dcMotor.get("RightFront");
-        bl_motor = hardwareMap.dcMotor.get("LeftRear");
-        br_motor = hardwareMap.dcMotor.get("RightRear");
+        motors[0] = hardwareMap.dcMotor.get("LeftFront");
+        motors[1] = hardwareMap.dcMotor.get("RightFront");
+        motors[2] = hardwareMap.dcMotor.get("LeftRear");
+        motors[3] = hardwareMap.dcMotor.get("RightRear");
 
-        fl_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fl_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fr_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        bl_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        br_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        fl_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        fr_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bl_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        br_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        for(int i = 0; i < 4; i++){
+            motors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motors[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
 
         waitForStart();
 
         // 304.8 mm is how much we want to move
         // 0.22165663 mm is the length of one tick
 
+        //old code
         double distance = 304.8;
+        for(int i = 0; i < 4; i++){
+            motors[i].setTargetPosition((int)(distance / TICK_LENGTH));
+        }
+        motors[0].setPower(-1);
+        motors[1].setPower(1);
+        motors[2].setPower(1);
+        motors[3].setPower(-1);
+        moving();
+        motors[0].setPower(0);
+        motors[1].setPower(0);
+        motors[2].setPower(0);
+        motors[3].setPower(0);
 
-        fl_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        fr_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        bl_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        br_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //new Code
+        move(-90, 304.8, 1);
+    }
 
-        fl_motor.setTargetPosition((int)(distance / TICK_LENGTH));
-        fr_motor.setTargetPosition((int)(distance / TICK_LENGTH));
-        bl_motor.setTargetPosition((int)(distance / TICK_LENGTH));
-        br_motor.setTargetPosition((int)(distance / TICK_LENGTH));
+    void move(double degrees, double distance, double power){
+        double radians = degrees * Math.PI / 180;
+        double x = power * Math.cos(radians);
+        double y = power * Math.sin(radians);
+        double[] pows = TrigDriveCode.calculateMotorPower(x, y, 0);
 
-        fl_motor.setPower(-1);
-        fr_motor.setPower(1);
-        bl_motor.setPower(1);
-        br_motor.setPower(-1);
+        for(int i = 0; i < 4; i++){
+            motors[i].setTargetPosition((int)(distance / TICK_LENGTH));
+            motors[i].setPower(pows[i]);
+        }
 
         moving();
 
-        fl_motor.setPower(0);
-        fr_motor.setPower(0);
-        bl_motor.setPower(0);
-        br_motor.setPower(0);
+        for(int i = 0; i < 4; i++){
+            motors[i].setPower(0);
+        }
     }
 
     void moving(){
-        while (fl_motor.isBusy() || fr_motor.isBusy() || bl_motor.isBusy() || br_motor.isBusy()){}
+        while (motors[0].isBusy() || motors[1].isBusy() || motors[2].isBusy() || motors[3].isBusy()){}
         return;
     }
-
 }
