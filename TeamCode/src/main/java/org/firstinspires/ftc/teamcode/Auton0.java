197cd2ce05892cbad04778d9aa05f2fd9358606f
ukshat <33008157+ukshat@users.noreply.github.com>
2020-11-09 16:25:49 -0800

    Autonomous documented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton0.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton0.java
@@ -13,43 +13,47 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 @Autonomous(name = "Autonomous")
 public class Auton0 extends LinearOpMode {
 
+    // length of a tile
     static final double TILE_LENGTH = 23.5;
 
+    // number of ticks in one inch
     static final double TICKS_PER_INCH = 34.2795262044082261656;
+
+    // constant for offset between front/back to left/right
     static final double HORIZONTAL_STRAFE = 36.0 / 30.75;
 
+    // stores the current direction of the robot
     double currOrientation;
 
     DcMotor[/*Front Left, Front Right, Back Left, Back Right*/] motors = new DcMotor[4];
 
+    // Variables used to initialize gyro
     BNO055IMU imu;
     BNO055IMU.Parameters params;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        //init motors
+        // init gyro
         imu = (BNO055IMU) hardwareMap.get("imu");
 
+        // init motors
         motors[0] = hardwareMap.dcMotor.get("LeftFront");
         motors[1] = hardwareMap.dcMotor.get("RightFront");
         motors[2] = hardwareMap.dcMotor.get("LeftRear");
         motors[3] = hardwareMap.dcMotor.get("RightRear");
 
+        // init zero power behavior
         for (DcMotor motor : motors) motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // init gyro parameters
         params = new BNO055IMU.Parameters();
         params.angleUnit = BNO055IMU.AngleUnit.DEGREES;
         imu.initialize(params);
 
         waitForStart();
 
-
-
-
-
-
         //Move to stack
-        move(0, TILE_LENGTH * 1.5, 0.5, motors);
+        move(0, TILE_LENGTH * 1.5, 0.5);
 
         sleep(500);
 
@@ -58,30 +62,21 @@ public class Auton0 extends LinearOpMode {
         sleep(500);
 
         //move to Launch position
-        move(0, TILE_LENGTH * 1, 0.5, motors);
+        move(0, TILE_LENGTH * 1, 0.5);
 
         sleep(500);
-//
-//        //launch all rings
-//        rotate(-(21.66115082 - 00.00000000));
-//        launch();
-//        rotate(-(26.72736173 - 21.66115082));
-//        launch();
-//        rotate(-(31.38022942 - 26.72736173));
-//        launch();
-//        rotate(31.38022942);
 
         switch(rings) {
             case 0 :
-                move(0, TILE_LENGTH * 0.5, 0.5, motors);
+                move(0, TILE_LENGTH * 0.5, 0.5);
                 break;
 
             case 1:
-                move(0, TILE_LENGTH * 1.5, 0.5, motors);
+                move(0, TILE_LENGTH * 1.5, 0.5);
                 break;
 
             case 4:
-                move(0, TILE_LENGTH * 2.5, 0.5, motors);
+                move(0, TILE_LENGTH * 2.5, 0.5);
                 break;
         }
 
@@ -89,15 +84,15 @@ public class Auton0 extends LinearOpMode {
 
         switch(rings) {
             case 0 :
-                move(2, TILE_LENGTH * 0, 0.5, motors);
+                move(2, TILE_LENGTH * 0, 0.5);
                 break;
 
             case 1:
-                move(2, TILE_LENGTH * 1, 0.5, motors);
+                move(2, TILE_LENGTH * 1, 0.5);
                 break;
 
             case 4:
-                move(2, TILE_LENGTH * 2, 0.5, motors);
+                move(2, TILE_LENGTH * 2, 0.5);
                 break;
         }
     }
@@ -110,11 +105,15 @@ public class Auton0 extends LinearOpMode {
         return 4;
     }
 
-    static void move(int config, double distance, double speed, DcMotor[] motors){
+    void move(int config, double distance, double speed){
         setDirection(config, motors);
         for(int i = 0; i < 4; i++){
+            // reset encoders
             motors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            // set power to motors
             motors[i].setPower(speed);
+            // set target position
+            // if left/right, multiply by horizontal strafe constant
             motors[i].setTargetPosition((int)(distance * TICKS_PER_INCH * (config % 2 == 1 ? HORIZONTAL_STRAFE : 1)));
             motors[i].setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
@@ -128,77 +127,125 @@ public class Auton0 extends LinearOpMode {
     }
 
     static void moving(DcMotor[] motors, boolean slowDown){
+        // stores the total distance to move
         final int totalTick = motors[0].getTargetPosition();
 
+        // continue the while loop until all motors complete movement
         while (motors[0].isBusy() || motors[1].isBusy() || motors[2].isBusy() || motors[3].isBusy()){
+            // delay
             try {Thread.sleep(75);} catch (InterruptedException e) {} //sleep
+            // if we choose to, increment speed gradually to minimize jerking motion
             if(slowDown){
                 int currPos = motors[0].getCurrentPosition();
-
                 double pow = f(currPos, totalTick);
-
                 for(DcMotor motor : motors) motor.setPower(pow);
             }
         }
     }
 
+    // function to calculate power for motors given distance and current distance to ensure gradual increase and decrease in motor powers
+    // an equation for graph of powers assuming that the highest power is 0.5; graph it in Desmos to see
     static double f(int x, int n){
         return -Math.pow((2.8 * Math.pow(x - n / 2, 2)) / (n * n), 2) + 0.5;
     }
 
+    /**
+     * rotates the robot a certain amount of degrees at a given speed
+     *
+     * @params degrees amount in degrees to rotate the robot (must be between -180 and 180)
+     * @params power speed at which motors will run -- affects speed of rotation
+     * */
+    void rotate(double degrees){
 
-    void rotate(double degrees, DcMotor[] motors, BNO055IMU imu){
-
+        // Create the object used to keep track of the current angle of the robot
         Orientation orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        // The angle at which the robot starts at
         double angle = orientation.firstAngle;
+
+        // The amount we want to turn
         double firstDegrees = degrees;
 
+        // This is our target position
         degrees += currOrientation;
 
+        // The orientation where the robot starts
         final double startAngle = angle;
+
+        // Reset the motors to run using encoders
         for(int i = 0; i < 4; i++) {
             motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
+
+        // If we are turning clockwise
+
         if (firstDegrees < 0){
             setDirection(4, motors );
+            // While we are not at the target position
+            // Multily by 0.925 to account for jerk
             while (angle > degrees * 0.925 && opModeIsActive()){
+                // Updating the object that keeps track of orientation
                 orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                // Updates the variable which stores the current direction of the robot
                 angle = orientation.firstAngle;
+                // Sets all motor powers to 0.2
                 for(int i = 0; i < 4; i++) motors[i].setPower(0.2);
+                // Delay
                 sleep(20);
             }
 
         }
 
+        // If we are turning counterclockwise
         else {
             setDirection(5, motors);
             while (angle < degrees * 0.925){
+                // Updating the object that keeps track of orientation
                 orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                // Updates the variable which stores the current direction of the robot
                 angle = orientation.firstAngle;
-                for (int i = 0; i < 4; i++){
-                    motors[i].setPower(0.2);
-                }
+                // Sets all motor powers to 0.2
+                for(int i = 0; i < 4; i++) motors[i].setPower(0.2);
+                // Delay
                 sleep(20);
             }
 
         }
+        // Stop the robot
         for (DcMotor motor: motors){
             motor.setPower(0);
         }
 
+        // Update the direction of the robot
         currOrientation = degrees;
 
     }
-
+    /**
+     * Configs:
+     *  0: forward
+     *  1: right
+     *  2: backward
+     *  3: left
+     *  4: clockwise
+     *  5: counterclockwise
+     * @param config
+     * @param motors
+     */
     static void setDirection(int config, DcMotor[] motors){
 
         for(int i = 0; i < 4; i++){
             motors[i].setDirection(DcMotor.Direction.FORWARD);
-            if (config == 0 && i % 2 == 0) motors[i].setDirection(DcMotor.Direction.REVERSE);
-            else if (config == 1 && i >= 2) motors[i].setDirection(DcMotor.Direction.REVERSE);
-            else if (config == 2 && i % 2 == 1) motors[i].setDirection(DcMotor.Direction.REVERSE);
-            else if (config == 3 && i <= 1) motors[i].setDirection(DcMotor.Direction.REVERSE);
-            else if (config == 4) motors[i].setDirection(DcMotor.Direction.REVERSE);
+            if (config == 0 && i % 2 == 0) {
+                motors[i].setDirection(DcMotor.Direction.REVERSE);
+            } else if (config == 1 && i >= 2) {
+                motors[i].setDirection(DcMotor.Direction.REVERSE);
+            } else if (config == 2 && i % 2 == 1) {
+                motors[i].setDirection(DcMotor.Direction.REVERSE);
+            } else if (config == 3 && i <= 1) {
+                motors[i].setDirection(DcMotor.Direction.REVERSE);
+            } else if (config == 4) {
+                motors[i].setDirection(DcMotor.Direction.REVERSE);
+            }
         }
 
     }
