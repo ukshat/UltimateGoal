219a3ff66b0b8fd8b05c55603242418e82b808d1
ukshat <33008157+ukshat@users.noreply.github.com>
2020-11-18 14:17:08 -0800

    Changed color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingDetermination.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingDetermination.java
@@ -1,51 +1,79 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 
 public class RingDetermination extends LinearOpMode {
-
-    BufferedImage abc = new BufferedImage(200, 200, 1);
 
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
     OpenCvCamera webcam;
+    private Bitmap image;
 
     public void initialize() {
-        VideoCapture capture = new VideoCapture(1);
-        capture.open(0);
-
-        Mat image = new Mat();
-        capture.read(image);
-
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.openCameraDevice();
+        webcam.setPipeline(new UselessGreenBoxDrawingPipeline());
+        webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
     }
 
-    private int ringCount;
-
-    public int countRings(Mat input) {
-        initialize();
-
-        return ringCount;
+    public static Bitmap Mat2BufferedImage(Mat mat) {
+        //Encoding the image
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", mat, matOfByte);
+        //Storing the encoded Mat in a byte array
+        byte[] byteArray = matOfByte.toArray();
+        //Preparing the Bitmap Image
+        final Bitmap image = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+        return image;
     }
 
     @Override
     public void runOpMode() throws InterruptedException {
-//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         initialize();
         waitForStart();
 
         while (opModeIsActive()) {
+            telemetry.addData("Image capture", image);
+            telemetry.update();
+
             sleep(100);
         }
 
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    class UselessGreenBoxDrawingPipeline extends OpenCvPipeline {
+        @Override
+        public Mat processFrame(Mat input) {
+            image = Mat2BufferedImage(input);
+
+            return input;
+        }
     }
 
 }
