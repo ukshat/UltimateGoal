12cd27f956c1e9baf6b7837a7ed9231d9777184b
ukshat <33008157+ukshat@users.noreply.github.com>
2021-01-02 15:01:39 -0800

    Edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleTest.java
@@ -14,21 +14,24 @@ public class WobbleTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        wobble = hardwareMap.dcMotor.get("");
-        claw = hardwareMap.servo.get("");
+        wobble = hardwareMap.dcMotor.get("wobblemotor");
+        claw = hardwareMap.servo.get("wobbleservo");
         wobble.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        claw.scaleRange(0, 1);
 
         waitForStart();
 
         wobble.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        wobble.setTargetPosition((int)(288.0 * 26/10/180 * (162.47-90.0)));
-        wobble.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        wobble.setPower(0.1);
-        while (wobble.isBusy() && opModeIsActive()){
-            sleep(20);
-        }
-        wobble.setPower(0);
+//        wobble.setTargetPosition((int)(288.0 * 26/10/180 * (162.47-90.0)));
+//        wobble.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        wobble.setPower(0.1);
+//        while (wobble.isBusy() && opModeIsActive()){
+//            sleep(20);
+//        }
+//        wobble.setPower(0);
+        claw.setPosition(0);
+        sleep(1000);
         claw.setPosition(1);
 
     }
