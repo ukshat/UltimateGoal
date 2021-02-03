1e7a759f846af12e87500014672779202fcff956
Yash Aggarwal <yash.aggy@gmail.com>
2021-02-03 14:50:31 -0800

    made shooter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Basic: Intake", group="Linear Opmode")
+//@Disabled
+
+public class Shooter extends LinearOpMode {
+
+    //declare OpMode members
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor shooter;
+
+    //@Override
+    public void runOpMode() {
+        // Find each motor on the hardware map
+        shooter = hardwareMap.dcMotor.get("intake");
+
+        //wait for driver to press play
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()){
+
+            try {Thread.sleep(25);} catch (InterruptedException e) {} //sleep
+
+            double shootingPower = -gamepad1.right_trigger;
+
+            // if gamepad is equal to a it sets the button for the intake to a
+            if(shootingPower < -0.4){
+                shooter.setPower(shootingPower);
+            }
+
+            telemetry.addData("shooting wheel motor", shootingPower + "\n");
+            telemetry.update();
+        }
+
+    }
+}
