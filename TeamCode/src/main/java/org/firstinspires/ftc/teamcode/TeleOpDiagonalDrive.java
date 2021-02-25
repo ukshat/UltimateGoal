6c729343ed85f3547b0ba2a7c951b7f13e31de1f
Yash Aggarwal <yash.aggy@gmail.com>
2021-02-24 16:23:07 -0800

    diagonal drive for tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDiagonalDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDiagonalDrive.java
@@ -27,6 +27,12 @@ public class TeleOpDiagonalDrive extends LinearOpMode {
         motors[2] = (DcMotorEx) hardwareMap.dcMotor.get("LeftRear");
         motors[3] = (DcMotorEx) hardwareMap.dcMotor.get("RightRear");
 
+        double degrees = Math.atan(80/34);
+        double distance = Math.sqrt(34*34 + 80*80);
+        double speed = 0.5;       
+
+        move(degrees, distance, speed);
+
     }
 
     double map(double from) { return from / HORIZONTAL_STRAFE; }
